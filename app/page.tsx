e74ac4c897590a311fa877dba3e8fc89b9be43d1
Yaphet Abraha <yaphet.abraha@gmail.com>
2025-01-06 11:56:09 -0600

    refactor to simplify code

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,13 @@
-import { getImageProps } from "next/image";
-import Background from "../assets/background.webp";
-import MiddleFrame from "../assets/frame-mdl.webp";
-
-function getBackgroundImage(srcSet = "") {
-  const imageSet = srcSet
-    .split(", ")
-    .map((str) => {
-      const [url, dpi] = str.split(" ");
-      return `url("${url}") ${dpi}`;
-    })
-    .join(", ");
-  return `image-set(${imageSet})`;
-}
-
 export default function Home() {
-  const {
-    props: { srcSet: backgroundSrcSet },
-  } = getImageProps({
-    alt: "",
-    src: Background,
-  });
-  const backgroundImage = getBackgroundImage(backgroundSrcSet);
-
-  const {
-    props: { srcSet: middleFrameSrcSet },
-  } = getImageProps({
-    alt: "",
-    src: MiddleFrame,
-  });
-  const middleFrameBackgroundImage = getBackgroundImage(middleFrameSrcSet);
-
   return (
-    <main
-      className="relative"
-      style={{ backgroundImage, backgroundSize: "100%", height: "100vh" }}
-    >
-      <div>Delicious in dungeon</div>
-      <div
-        className="body-main-frame absolute w-full top-0 left-0  h-lvh pointer-events-none "
-        style={{
-          backgroundImage: middleFrameBackgroundImage,
-          backgroundSize: "100%",
-        }}
-      />
-      <div className="w-[95%] mx-auto p-10 text-center">
-        <h2 className="scroll-m-20  pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          Danjon <span className="text-primary-foreground">Meshi</span>
-        </h2>
+    <main id="main-backdrop" className="relative">
+      <div className="part-body-main relative p-4">
+        <div className="body-main-frame absolute w-full top-0 left-0  h-lvh pointer-events-none " />
+        <div className="w-[95%] mx-auto p-10 text-center">
+          <h2 className="scroll-m-20  text-3xl font-semibold tracking-tight first:mt-0">
+            Danjon <span className="text-primary-foreground">Meshi</span>
+          </h2>
+        </div>
       </div>
     </main>
   );
